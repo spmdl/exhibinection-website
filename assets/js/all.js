68ffa0724bc67b810aaba7a02d428f1d4004401d
Spm-Eng <dlspm0512@gmail.com>
2021-08-30 22:44:59 +0800

    Update 2021-08-30T14:44:56.700Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,13 +4,13 @@ var orderInfo = document.querySelector('.order-info-js');
 var formWidth = document.querySelector('form');
 window.addEventListener('resize', function () {
   if (orderInfo) {
-    formWidth.style.width = orderInfo.offsetWidth + "px";
+    formWidth.style.width = orderInfo.offsetWidth - 30 + "px";
   }
 });
 
 (function () {
   if (orderInfo) {
-    formWidth.style.width = orderInfo.offsetWidth + "px";
+    formWidth.style.width = orderInfo.offsetWidth - 30 + "px";
   }
 })();
 //# sourceMappingURL=all.js.map
