c5a7aa7d0ec6882741a67090cad2ef3f6d3fad4e
Spm-Eng <dlspm0512@gmail.com>
2021-08-30 18:24:11 +0800

    Update 2021-08-30T10:24:08.041Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,15 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+(function () {
+  var orderInfo = document.querySelector('.order-info');
+  var formWidth = document.querySelector('form');
+
+  if (orderInfo) {
+    console.log(formWidth.offsetWidth);
+    console.log(orderInfo.offsetWidth);
+    formWidth.style.offsetWidth = orderInfo.offsetWidth;
+  }
+})(); // $(function() {
+//   console.log('Hello Bootstrap5');
+// });
 //# sourceMappingURL=all.js.map
