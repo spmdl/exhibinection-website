cbc9b847be2c5cd901849f48c035dcce9d5abe78
Spm-Eng <dlspm0512@gmail.com>
2021-08-30 18:23:52 +0800

    feat: position-md-fixed js change width

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,13 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+(function () {
+  var orderInfo = document.querySelector('.order-info'); 
+  var formWidth = document.querySelector('form');
+  if (orderInfo) {
+    console.log(formWidth.offsetWidth);
+    console.log(orderInfo.offsetWidth);
+    formWidth.style.offsetWidth = orderInfo.offsetWidth;
+  }
+}());
+
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
